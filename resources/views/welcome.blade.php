1836d37934c965cf79c0ab70a0a78abb1b64c620
Hernán Arica <hernanaricammm@gmail.com>
2022-09-22 15:31:41 -0300

    mend
    Se agrega una UI para buscar mas facilmente los endpoints

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		@vite('resources/css/app.css')
 		{{--		<link rel="stylesheet" href="" />--}}
+		@vite('resources/css/app.css')
 		<title>APi service providers</title>
 	</head>
 	<body>
