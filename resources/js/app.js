a2b798c522b4e2a979df7363cbbface5c94fa145
Hernán Arica <hernanaricammm@gmail.com>
2022-09-22 15:31:20 -0300

    Se agrega una UI para buscar mas facilmente los endpoints

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+const $buttons  = document.querySelectorAll('.btnTooltip');
+const $tooltips = document.querySelectorAll('.tooltip');
+
+console.log($tooltips);
+
+$buttons.forEach((btn, i) => {
+	btn.addEventListener('click', e => {
+		const url = e.currentTarget.getAttribute('data-url');
+		navigator.clipboard.writeText(url);
+		
+		$tooltips[i].classList.add('fade-in-up');
+		
+		setTimeout(() => {
+			$tooltips[i].classList.remove('fade-in-up');
+		}, 1000);
+	});
+});
+
+
