7c58785ec2f35c41aa15eb94713492ca981287b7
Hernán Arica <hernanaricammm@gmail.com>
2022-10-04 11:09:48 -0300

    Se crean las migraciones de las tablas, opportunities y deals

--- database/migrations/2022_09_30_183908_create_deals_table.php
+++ database/migrations/2022_09_30_183908_create_deals_table.php
@@ -4,28 +4,108 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('deals', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('deals');
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('deals', function (Blueprint $table) {
+			$table->integer('id')->primary();
+			$table->string('agency', 45)->nullable();
+			$table->integer('advertisers_id');
+			$table->integer('country_id')->nullable();
+			$table->integer('carriers_id')->nullable();
+			$table->integer('users_id');
+			$table->integer('campaign_categories_id');
+			$table->integer('devices_id')->nullable();
+			$table->integer('formats_id')->nullable();
+			$table->integer('actions_id')->nullable();
+			$table->enum('currency', ['usd', 'ars', 'eur', 'gbp', 'brl', 'mxn', 'clp', 'cop']);
+			$table->timestamp('start_date')->useCurrent();
+			$table->timestamp('end_date')->nullable();
+			$table->string('goal');
+			$table->enum('business_model', ['branding', 'performance', 'exchange', 'branding development', 'augmented reality', 'move pmp']);
+			$table->decimal('budget', 11, 2)->nullable();
+			$table->decimal('agency_commission', 11, 2)->default(0.00);
+			$table->enum('model_adv', ['cpa', 'cpc', 'cpm', 'cpi', 'cpv', 'cpl'])->default('CPM');
+			$table->string('conversion_flow', 70)->nullable();
+			$table->boolean('wifi')->default(0);
+			$table->text('dayparting')->nullable();
+			$table->text('campaigns_restrictions')->nullable();
+			$table->string('adserver', 70)->nullable();
+			$table->string('net_payment', 128);
+			$table->string('contact_com', 128);
+			$table->string('email_com', 128);
+			$table->string('contact_adm', 128);
+			$table->string('email_adm', 128);
+			$table->text('comment')->nullable();
+			$table->text('inventory_conditions')->nullable();
+			$table->enum('entity', ['llc', 'srl', 'bvi', 'mx', 'sa', 'sas'])->comment("0: LLC, 1: SRL");
+			$table->string('external_operation', 45)->nullable();
+			$table->decimal('rate', 11, 2)->nullable();
+			$table->timestamp('date')->nullable();
+			$table->enum('status', ['active', 'pending', 'approved', 'rejected', 'archived'])->default('Active');
+			$table->string('flow', 45)->nullable();
+			$table->string('commercial_name', 128);
+			$table->string('address', 128);
+			$table->string('zip_code', 128);
+			$table->string('tax_id', 128)->comment("0:CPA,1:CPC,2:CPM,2:CPL");
+			$table->integer('ios_id')->nullable();
+			$table->string('state', 128);
+			$table->string('pdf_name', 128)->nullable();
+			$table->string('phone', 128)->nullable();
+			$table->string('product')->nullable();
+			$table->string('freq_cap', 45)->nullable();
+			$table->integer('imp_per_day')->nullable();
+			$table->integer('imp_total')->nullable();
+			$table->string('targeting')->nullable();
+			$table->string('sizes')->nullable();
+			$table->enum('channel', ['categories', 'sites specifics', 'ron'])->nullable();
+			$table->string('channel_description')->nullable();
+			$table->integer('sales_id');
+			$table->decimal('media_buying_perc', 11, 2)->default(0.00);
+			$table->decimal('exchange_rate', 11, 3)->default(0.000);
+			$table->enum('created_by_sector', ['client services', 'sales'])->nullable();
+			$table->enum('provider_type', ['affiliate', 'network'])->nullable();
+			$table->integer('goal_units')->default(0);
+			$table->boolean('client_report')->default(0);
+			$table->integer('version');
+			$table->string('version_comment', 45);
+			$table->enum('priority', ['low', 'medium', 'high', 'urgent'])->default('Low');
+			$table->boolean('telecom')->default(0);
+			$table->integer('agency_fee')->nullable();
+			$table->enum('tracking_method', ['s2s', 'pixel'])->nullable();
+			$table->string('pdf_name2', 128)->nullable();
+			$table->string('seat_id')->nullable();
+			$table->enum('materials_development', ['kickads', 'client'])->default('Client');
+			$table->integer('dsp_id')->nullable();
+			$table->enum('materials_upload', ['kickads', 'client'])->default('Client');
+			$table->string('other_dsp')->nullable();
+			$table->string('vertical')->nullable();
+			$table->foreign('actions_id', 'fk_deals_actions1')->references('id')->on('actions')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('advertisers_id', 'fk_deals_advertisers1')->references('id')->on('advertisers')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('campaign_categories_id', 'fk_deals_campaign_categories1')->references('id')->on('campaign_categories')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('carriers_id', 'fk_deals_carriers1')->references('id_carrier')->on('carriers')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('devices_id', 'fk_deals_devices1')->references('id')->on('devices')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('dsp_id', 'fk_deals_dsp1')->references('id')->on('dsp')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('formats_id', 'fk_deals_formats1')->references('id')->on('formats')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('country_id', 'fk_deals_geo_location1')->references('id_location')->on('geo_location')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('ios_id', 'fk_deals_ios1')->references('id')->on('ios')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('users_id', 'fk_deals_users1')->references('id')->on('users')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('sales_id', 'fk_deals_users2')->references('id')->on('users')->onDelete('restrict')->onUpdate('restrict');
+		});
+	}
+	
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('deals');
+	}
 };
--- database/migrations/2022_09_30_183945_create_opportunities_table.php
+++ database/migrations/2022_09_30_183945_create_opportunities_table.php
@@ -4,28 +4,86 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('opportunities', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('opportunities');
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('opportunities', function (Blueprint $table) {
+			$table->integer('id')->primary();
+			$table->integer('carriers_id')->nullable();
+			$table->decimal('rate', 11, 2)->nullable();
+			$table->enum('model_adv', ['cpa', 'cpc', 'cpm', 'cpi', 'cpv', 'cpl', 'revenue share']);
+			$table->string('product')->nullable();
+			$table->integer('account_manager_id')->nullable();
+			$table->text('comment')->nullable();
+			$table->integer('country_id')->nullable();
+			$table->tinyInteger('wifi')->nullable();
+			$table->decimal('budget', 11, 2)->nullable();
+			$table->string('server_to_server', 45)->default('ktoken');
+			$table->timestamp('startDate')->nullable();
+			$table->timestamp('endDate')->nullable();
+			$table->integer('ios_id');
+			$table->string('freq_cap', 45)->nullable();
+			$table->integer('imp_per_day')->nullable();
+			$table->integer('imp_total')->nullable();
+			$table->string('targeting')->nullable();
+			$table->string('sizes')->nullable();
+			$table->enum('channel', ['categories', 'sites specifics', 'ron'])->nullable();
+			$table->string('channel_description')->nullable();
+			$table->enum('status', ['active', 'inactive', 'archived']);
+			$table->integer('version');
+			$table->boolean('closed_deal')->default(0);
+			$table->decimal('close_amount', 11, 2)->default(0.00);
+			$table->decimal('agency_commission', 11, 2)->default(0.00);
+			$table->integer('sem_analyst_id')->nullable();
+			$table->integer('commercial_id')->nullable();
+			$table->string('comment_internal')->nullable();
+			$table->enum('business_model', ['branding', 'performance', 'exchange', 'video'])->nullable();
+			$table->string('pdf_client_order', 128)->nullable();
+			$table->integer('deals_id')->nullable();
+			$table->text('target_url')->nullable();
+			$table->text('materials')->nullable();
+			$table->string('placeholder_publisher', 45)->default('pub_id');
+			$table->integer('goal_units')->default(0);
+			$table->integer('devices_id')->nullable();
+			$table->integer('actions_id')->nullable();
+			$table->integer('formats_id')->nullable();
+			$table->string('placeholder_source', 45)->nullable();
+			$table->string('placeholder_device_id', 45)->nullable();
+			$table->string('placeholder_gaid', 45)->nullable();
+			$table->string('placeholder_idfa', 45)->nullable();
+			$table->string('placeholder_provider_id', 45)->nullable();
+			$table->decimal('cpm_client_price', 11, 2)->nullable();
+			$table->decimal('cpm_floor_price_mow', 11, 2)->nullable();
+			$table->boolean('receptivity')->default(0);
+			$table->string('fee_tech', 45)->nullable();
+			$table->string('ctr', 45)->nullable();
+			$table->string('viewability', 45)->nullable();
+			$table->string('url', 512)->nullable();
+			$table->foreign('actions_id', 'fk_opportunities_actions1')->references('id')->on('actions')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('carriers_id', 'fk_opportunities_carriers')->references('id_carrier')->on('carriers')->onDelete('set NULL')->onUpdate('restrict');
+			$table->foreign('country_id', 'fk_opportunities_country')->references('id_location')->on('geo_location')->onDelete('set NULL')->onUpdate('restrict');
+			$table->foreign('deals_id', 'fk_opportunities_deals1')->references('id')->on('deals')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('devices_id', 'fk_opportunities_devices1')->references('id')->on('devices')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('formats_id', 'fk_opportunities_formats1')->references('id')->on('formats')->onDelete('restrict')->onUpdate('restrict');
+			$table->foreign('ios_id', 'fk_opportunities_ios')->references('id')->on('ios')->onDelete('cascade')->onUpdate('restrict');
+			$table->foreign('account_manager_id', 'fk_opportunities_users')->references('id')->on('users')->onDelete('set NULL')->onUpdate('restrict');
+			$table->foreign('sem_analyst_id', 'fk_opportunities_users1')->references('id')->on('users')->onDelete('set NULL')->onUpdate('restrict');
+			$table->foreign('commercial_id', 'fk_opportunities_users2')->references('id')->on('users')->onDelete('set NULL')->onUpdate('restrict');
+		});
+	}
+	
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('opportunities');
+	}
 };
